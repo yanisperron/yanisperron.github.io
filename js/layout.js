1a21721d02316003120bb8fd8beb948043e08d24
Yanis Perron <nisperron@gmail.com>
2025-12-02 18:15:18 +0100

    Create layout.js

---
+++ js/layout.js
@@ -0,0 +1,13 @@
+const nav = document.querySelector('.menu');
+const u = document.querySelector('.underline');
+
+nav.addEventListener('mouseover', e => {
+  if(e.target.tagName !== 'A') return;
+  const r = e.target.getBoundingClientRect();
+  u.style.width = r.width + 'px';
+  u.style.left = e.target.offsetLeft + r.width/2 + 'px';
+});
+
+nav.addEventListener('mouseleave', () => {
+  u.style.width = 0;
+});
