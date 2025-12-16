2fd2a72ffac1696146795355dd63bd1013e01489
Yanis Perron <nisperron@gmail.com>
2025-12-16 12:19:24 +0100

    Update layout.js

--- js/layout.js
+++ js/layout.js
@@ -1,13 +1,38 @@
 const nav = document.querySelector('.nav-wrapper');
 const u = document.querySelector('.underline');
+const links = nav.querySelectorAll('a');
+const currentPage = window.location.pathname.split('/').pop();
+const images = ["https://i.imgur.com/t6wMlZt.jpeg","https://i.imgur.com/B3MnaEZ.jpeg","https://i.imgur.com/sEuOblP.jpeg","https://i.imgur.com/egC7PA8.jpeg","https://i.imgur.com/fkIATkz.jpeg"];
+const randomImg = images[Math.floor(Math.random() * images.length)];
+console.log(randomImg);
 
-nav.addEventListener('mouseover', e => {
-  if(e.target.tagName !== 'A') return;
-  const r = e.target.getBoundingClientRect();
-  u.style.width = r.width + 'px';
-  u.style.left = e.target.offsetLeft + r.width/2 + 'px';
+// EFFET SUR LA NAVBAR
+
+let activeLink = null;
+links.forEach(link => {
+  if (link.getAttribute('href') === currentPage) activeLink = link;
 });
 
-nav.addEventListener('mouseleave', () => {
-  u.style.width = 0;
+function setUnderline(el) {
+  if (!el) return;
+  u.style.width = el.offsetWidth + 'px';
+  u.style.left = el.offsetLeft + 'px';
+}
+
+window.addEventListener('load', () => setUnderline(activeLink));
+nav.addEventListener('mouseover', e => {
+  if (e.target.tagName === 'A') setUnderline(e.target);
+});
+nav.addEventListener('mouseleave', () => setUnderline(activeLink));
+window.addEventListener('resize', () => setUnderline(activeLink));
+
+
+// IMAGE RANDOM
+
+document.querySelector("img").src = randomImg;
+
+window.addEventListener("scroll", () => {
+  const nav = document.querySelector(".nav-wrapper");
+  if (window.scrollY > 50) nav.classList.add("scrolled");
+  else nav.classList.remove("scrolled");
 });
